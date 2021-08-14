2c7b5f0713842a14524efb5fded796296ad98a4d
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-08-14 18:18:00 -0400

    working show function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
-const consoleTable = require('console.table');
+const cTable = require('console.table');
+const inquirer = require('inquirer'); 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,18 +16,60 @@ const db = mysql.createConnection(
     password: 'shnood6176',
     database: 'employee_db'
   },
-  console.log('Connected to employee database')
+
 );
 
-
-  db.query(`SELECT * FROM employee`, (err, rows) => {
-    console.log(rows);
-  });
-
-app.use((req, res) => {
-    res.status(404).end();
-  });
-
-app.listen(PORT, () => {
+db.connect((err) => {
+  if (err) throw err;
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+  welcome();
+});
+
+welcome = () => {
+  console.log("***********************************")
+  console.log("*  WELCOME TO EMPLOYEE TRACKER    *")
+  console.log("***********************************")
+  menu();
+};
+
+const menu = () => {
+  inquirer.prompt ([
+    {
+      type: 'list',
+      name: 'choices', 
+      message: 'What would you like to do?',
+      choices: ['View all departments', 
+                'View all roles', 
+                'View all employees', 
+                'Add a department', 
+                'Add a role', 
+                'Add an employee', 
+                'Update an employee role',
+                'Finished']
+    }
+  ])
+  .then((answers) => {
+    const { choices } = answers; 
+
+    if (choices === "View all departments") {
+      viewDepartments();
+    }
+    if (choices === "Finished") {
+      console.log('Press Ctrl+C to exit application')
+      db.end()
+    }
+  })
+};
+
+  const viewDepartments = () => {
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      console.table(res);
+      menu();
+    });
+  };
